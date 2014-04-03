da06210c9b7503d3f44b919db8565a5f8626c876
Chris LaRose <cjlarose@gmail.com>
2014-04-02 20:03:50 -0700

    Encryption on whole message

--- dsa.hs
+++ dsa.hs
@@ -1,3 +1,4 @@
+import Data.Char (ord, chr)
 import Data.Bits (shiftL, shiftR, (.&.), (.|.), xor)
 
 generateKeys :: Int -> [Int]
@@ -9,18 +10,25 @@ expand :: Int -> Int
 expand n = (shiftL (n .&. 0x30) 2) .|. 
            (shiftL (n .&. 0x4 ) 3) .|. 
            (shiftL (n .&. 0xC) 1) .|. 
-           (shiftR (n .&. 0x8) 1) .|. 
+           (shiftR (n .&. 0x8) 1) .|.
            (n .&. 0x3)
 
--- splits int n into two ints of bitlength k
-split :: Int -> Int -> (Int, Int)
-split n k = (left,  right)
-    where left = n `shiftR` k
-          right = n .&. (( 1 `shiftL` k ) - 1)
+-- split int n into len ints of bitlength k
+splitList :: Int -> Int -> Int -> [Int]
+splitList n len bitlength = snd $ foldl f (n, []) [0..(len-1)]
+    where f (x, xs) _ = (x `shiftR` bitlength, (x .&. mask) : xs)
+          mask        = (1 `shiftL` bitlength) - 1
 
--- concatenate a pair of ints of bitlength k
+split :: Int -> Int -> (Int, Int)
+split n k = (left, right)
+    where left:right:_ = splitList n 2 k
+
+-- concatenate a list of ints of bitlength k
+joinList :: [Int] -> Int -> Int
+joinList xs k = foldl (\a b -> (a `shiftL` k) .|. b) 0 xs
+
 join :: (Int, Int) -> Int -> Int
-join (a, b) k = (a `shiftL` k) .|. b
+join (a, b) k = joinList [a, b] k
 
 -- 8 bits -> 6 bits
 sBoxLookup :: Int -> Int
@@ -33,4 +41,22 @@ dsaRound n k_i = join (r, newR) 6
     where (l, r) = split n 6
           newR = (xor l) . sBoxLookup . (xor k_i) . expand $ r
 
-dsaEncrypt n k = foldl dsaRound n (generateKeys k)
+-- encrypt 12 bit plaintex n with key k
+dsaEncryptBlock k n = foldl dsaRound n (generateKeys k)
+
+group _ [] = []
+group n l
+    | n > 0 = (take n l) : (group n (drop n l))
+    | otherwise = error "Negative n"
+
+joinTriple triple = [a,b]
+    where (a, b) = split (joinList triple 8) 12
+
+-- chunk input characters into 12-bit chunks
+chunk xs = concat . map joinTriple . group 3 . map ord $ xs
+
+-- given 12-bit blocks, turn back into bytes
+unchunk = concat . map f . group 2
+    where f (a:b:_) = splitList (join (a, b) 12) 3 8
+
+dsaEncrypt k xs = unchunk . map (dsaEncryptBlock k) . chunk $ xs
