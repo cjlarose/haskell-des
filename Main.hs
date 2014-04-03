132b6a881bb1c4bf1cf4034a0d1c64c5fa2c784b
Chris LaRose <cjlarose@gmail.com>
2014-04-02 23:51:59 -0700

    Argument parsing

--- Main.hs
+++ Main.hs
@@ -1,9 +1,20 @@
 import DES
 import Data.Char (chr)
 import qualified Data.ByteString.Char8 as B
+import System.Environment (getArgs)
+import System.IO (stderr, hPutStrLn)
 
 main :: IO ()
 main = do
-    plaintext <- B.getLine
-    let encrypted = desEncrypt 511 . B.unpack $ plaintext
-    B.putStr . B.pack . map chr $ encrypted
+    (action:keyStr:_) <- getArgs
+    let key = read keyStr
+    if action == "encrypt"
+        then do
+            plaintext <- B.getLine
+            let encrypted = desEncrypt key . B.unpack $ plaintext
+            B.putStr . B.pack . map chr $ encrypted
+        else
+            if action == "decrypt"
+                then putStr "decrypt not implemented"
+            else
+                hPutStrLn stderr "not implemented"
