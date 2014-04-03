7fc9070b44652a259a6d588e3f892cc35572f3eb
Chris LaRose <cjlarose@gmail.com>
2014-04-03 10:03:33 -0700

    Changed argument order of bitwise utility functions

--- DES.hs
+++ DES.hs
@@ -5,7 +5,7 @@ import Data.Bits (shiftL, shiftR, (.&.), (.|.), xor)
 import Data.Tuple (swap)
 
 generateKeys :: Int -> [Int]
-generateKeys key = f (join (key, key) 9) 4
+generateKeys key = f (join 9 (key, key)) 4
     where f k len = map (getKey k) [0..(len-1)]
           getKey k i = (shiftR k (10 - i)) .&. 255
 
@@ -18,25 +18,25 @@ expand n = (shiftL (n .&. 0x30) 2) .|.
 
 -- split int n into len ints of bitlength k
 splitList :: Int -> Int -> Int -> [Int]
-splitList n len bitlength = snd $ foldl f (n, []) [0..(len-1)]
+splitList bitlength len n = snd $ foldl f (n, []) [0..(len-1)]
     where f (x, xs) _ = (x `shiftR` bitlength, (x .&. mask) : xs)
           mask        = (1 `shiftL` bitlength) - 1
 
 split :: Int -> Int -> (Int, Int)
-split n k = (left, right)
-    where left:right:_ = splitList n 2 k
+split k n = (left, right)
+    where left:right:_ = splitList k 2 n
 
 -- concatenate a list of ints of bitlength k
-joinList :: [Int] -> Int -> Int
-joinList xs k = foldl (\a b -> (a `shiftL` k) .|. b) 0 xs
+joinList :: Int -> [Int] -> Int
+joinList k xs = foldl (\a b -> (a `shiftL` k) .|. b) 0 xs
 
-join :: (Int, Int) -> Int -> Int
-join (a, b) k = joinList [a, b] k
+join :: Int -> (Int, Int) -> Int
+join k (a, b) = joinList k [a, b]
 
 -- 8 bits -> 6 bits
 sBoxLookup :: Int -> Int
-sBoxLookup n = join (s1 !! l, s2 !! r) 3
-    where (l, r) = split n 4
+sBoxLookup n = join 3 (s1 !! l, s2 !! r)
+    where (l, r) = split 4 n
           s1 = [5, 2, 1, 6, 3, 4, 7, 0, 1, 4, 6, 2, 0, 7, 5, 3]
           s2 = [4, 0, 6, 5, 7, 1, 3, 2, 5, 3, 0, 7, 6, 2, 1, 4]
 
@@ -44,7 +44,7 @@ desRound (l, r) k_i = (r, newR)
     where newR = (xor l) . sBoxLookup . (xor k_i) . expand $ r
 
 -- encrypt 12 bit plaintex n with keys
-desEncryptBlock keys n = join (foldl desRound (split n 6) keys) 6
+desEncryptBlock keys n = join 6 (foldl desRound (split 6 n) keys)
 
 group _ [] = []
 group n l
@@ -52,14 +52,14 @@ group n l
     | otherwise = error "Negative n"
 
 joinTriple triple = [a,b]
-    where (a, b) = split (joinList triple 8) 12
+    where (a, b) = split 12 (joinList 8 triple)
 
 -- chunk input characters into 12-bit chunks
 chunk xs = concat . map joinTriple . group 3 . map ord $ xs
 
 -- given 12-bit blocks, turn back into bytes
 unchunk = concat . map f . group 2
-    where f (a:b:_) = splitList (join (a, b) 12) 3 8
+    where f (a:b:_) = splitList 8 3 (join 12 (a, b))
 
 desProcessInput f = unchunk . map f . chunk
 
