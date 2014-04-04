d384184bcd52449783050515354825f2ee13662f
Chris LaRose <cjlarose@gmail.com>
2014-04-04 15:13:46 -0700

    Fix types of desEncrypt and desDecrypt

--- DES.hs
+++ DES.hs
@@ -1,15 +1,13 @@
 module DES (desEncrypt, desDecrypt) where
 
 import Data.Char (ord, chr)
-import Data.Bits (shiftL, shiftR, (.&.), (.|.), xor)
+import Data.Bits (shiftL, shiftR, (.&.), (.|.), xor, Bits)
 import Data.Tuple (swap)
 
-generateKeys :: Int -> [Int]
 generateKeys key = f (join 9 (key, key)) 4
     where f k len = map (getKey k) [0..(len-1)]
           getKey k i = (shiftR k (10 - i)) .&. 0xFF
 
-expand :: Int -> Int
 expand n = (shiftL (n .&. 0x30) 2) .|. 
            (shiftL (n .&. 0x4 ) 3) .|. 
            (shiftL (n .&. 0xC) 1) .|. 
@@ -17,25 +15,20 @@ expand n = (shiftL (n .&. 0x30) 2) .|.
            (n .&. 0x3)
 
 -- split int n into len ints of bitlength k
-splitList :: Int -> Int -> Int -> [Int]
 splitList bitlength len n = map f [0..(len-1)]
     where f i = n `shiftR` ((len - i - 1) * bitlength)  .&. mask
           mask    = (1 `shiftL` bitlength) - 1
 
-split :: Int -> Int -> (Int, Int)
 split k n = (left, right)
     where left:right:_ = splitList k 2 n
 
 -- concatenate a list of ints of bitlength k
-joinList :: Int -> [Int] -> Int
 joinList k = foldl (\a b -> (a `shiftL` k) .|. b) 0
 
-join :: Int -> (Int, Int) -> Int
 join k (a, b) = joinList k [a, b]
 
 -- 8 bits -> 6 bits
-sBoxLookup :: Int -> Int
-sBoxLookup n = join 3 (s1 !! l, s2 !! r)
+sBoxLookup n = join 3 (s1 !! (fromIntegral l), s2 !! (fromIntegral r))
     where (l, r) = split 4 n
           s1 = [5, 2, 1, 6, 3, 4, 7, 0, 1, 4, 6, 2, 0, 7, 5, 3]
           s2 = [4, 0, 6, 5, 7, 1, 3, 2, 5, 3, 0, 7, 6, 2, 1, 4]
@@ -46,7 +39,6 @@ desRound (l, r) k_i = (r, newR)
 -- encrypt 12 bit plaintex n with keys
 desEncryptBlock keys n = join 6 $ foldl desRound (split 6 n) keys
 
-group :: Int -> [a] -> [[a]]
 group _ [] = []
 group n l
     | n > 0 = (take n l) : (group n (drop n l))
@@ -56,21 +48,20 @@ joinTriple triple = [a,b]
     where (a, b) = split 12 $ joinList 8 triple
 
 -- chunk input characters into 12-bit chunks
---chunk :: [Char] -> [Int]
---chunk :: B.ByteString -> [Int]
+chunk :: (Num t, Bits t) => [t] -> [t]
 chunk = concat . map joinTriple . group 3
 
 -- given 12-bit blocks, turn back into bytes
+unchunk :: (Num t, Bits t) => [t] -> [t]
 unchunk = concat . map f . group 2
     where f (a:b:_) = splitList 8 3 $ join 12 (a, b)
 
---desProcessInput :: (Int -> Int) -> [Char] -> [Int]
 desProcessInput f = unchunk . map f . chunk
 
---desEncrypt :: Int -> [Char] -> [Int]
+desEncrypt :: Int -> [Int] -> [Int]
 desEncrypt k = desProcessInput $ desEncryptBlock $ generateKeys k
 
 desDecryptBlock keys n = join 6 $ swap $ foldl desRound (swap $ split 6 n) keys
 
---desDecrypt :: Int -> [Char] -> [Int]
+desDecrypt :: Int -> [Int] -> [Int]
 desDecrypt k xs = desProcessInput (desDecryptBlock . reverse . generateKeys $ k) xs
